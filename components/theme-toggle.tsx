4360117dc073d0ab9e0f0d1bc95ed4df7e06cb17
Anirudh H S <anirudhb302005@gmail.com>
2026-09-05 00:27:29 +0530

    Simplify theme toggle icon layout

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -24,8 +24,10 @@ export function ThemeToggle() {
           aria-label="Toggle theme"
           suppressHydrationWarning
         >
-          <Sun className="h-4 w-4 scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-4 w-4 scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="flex items-center justify-center">
+            <Sun className="h-4 w-4" />
+            <Moon className="h-4 w-4" />
+          </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
